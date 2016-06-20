f56745288d3b1841859888b8703cd6f99b7414e2
batu <ba921@nyu.edu>
2016-06-20 10:49:04 -0400

    Siktir git has been run, carefull with this!

---
+++ summer2016/linearAlgebra/vid4_complex.py
@@ -0,0 +1,23 @@
+from plotting import plot;
+import math;
+
+## Question 1
+print(60*60*24*7)
+
+## Question 2
+print(2304811 -(2304811//47 * 47))
+
+## Question 3
+y = -9
+x = 1/2
+print (2**(y+1/2) if x+10 < 0 else 2**(y-1/2))
+
+
+## Question 4
+print({ x**2 for x in {1, 2, 3, 4, 5} })
+
+## Question 5
+print({ 2**x for x in {1, 2, 3, 4, 5} })
+
+## Question 6
+print(sum([20, 10, 15, 75]) / len([20, 10, 15, 75]))
